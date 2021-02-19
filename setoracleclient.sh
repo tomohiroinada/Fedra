5b750503fe8ef96609f5878b5770d138d26dac77
tomohiro <tomohiro@icrr.u-tokyo.ac.jp>
2021-02-19 21:40:28 +0900

    prepare setoracleclient.sh

---
+++ setoracleclient.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+$FEDRA_PATH=/Users/tomohiro/software/FASER/fedra
+	
+### required for occi (access from c++):
+export OCCIHOME=$FEDRA_PATH/instantclient
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OCCIHOME
+export PATH=$PATH:$OCCIHOME
+
+### required for $TNS_ADMIN/tnsnames.ora
+export TNS_ADMIN=$FEDRA_PATH/oracle
+
+### required for $ORACLE_HOME/sqplus/admin/glogon.sql  (optional)
+export ORACLE_HOME=$FEDRA_PATH/oracle

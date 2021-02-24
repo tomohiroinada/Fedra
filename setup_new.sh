8797eaba298e169340ef8ac0ce8aa20835104674
tomohiroinada <tomohiro@icrr.u-tokyo.ac.jp>
2021-02-25 02:01:02 +0900

    script to prepare pathes

---
+++ setup_new.sh
@@ -0,0 +1,4 @@
+export FEDRA_ROOT=/home/tomohiro/sim/FASER/virtualrec/Fedra
+export LD_LIBRARY_PATH=$FEDRA_ROOT/lib:${LD_LIBRARY_PATH}
+export PATH=${PATH}:$FEDRA_ROOT/bin
+export PYTHONPATH=${PYTHONPATH}:$FEDRA_ROOT/python
